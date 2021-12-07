bb5b8b9062170d040a7cc30e4a2c3e238902c23a
jevon <jevon.levin@gamil.com>
2021-12-07 22:18:18 +0700

    22nd commit

--- app/bee-portal/src/index.js
+++ app/bee-portal/src/index.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow, Menu, Tray } = require('electron')
+const { app, BrowserWindow, Tray, Menu } = require('electron')
 const path = require('path')
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) {
@@ -6,9 +6,10 @@ if (require('electron-squirrel-startup')) {
   app.quit()
 }
 
+let mainWindow = null
 const createWindow = () => {
   // Create the browser window.
-  const mainWindow = new BrowserWindow({
+  mainWindow = new BrowserWindow({
     width: 1050,
     height: 700,
     webPreferences: {
@@ -16,7 +17,6 @@ const createWindow = () => {
       contextIsolation: false,
     },
   })
-
   // and load the index.html of the app.
   mainWindow.loadFile(path.join(__dirname, './page/index.html'))
 
@@ -24,31 +24,6 @@ const createWindow = () => {
   mainWindow.webContents.openDevTools()
 
   mainWindow.setResizable(false)
-
-  mainWindow.on('close', function (event) {
-    event.preventDefault()
-    mainWindow.hide()
-  })
-  var contextMenu = Menu.buildFromTemplate([
-    {
-      label: 'Show App',
-      click: function () {
-        mainWindow.show()
-      },
-    },
-    {
-      label: 'Quit',
-      click: function () {
-        mainWindow.destroy()
-        app.quit()
-      },
-    },
-  ])
-
-  appIcon = new Tray('./logo/logo.png')
-  var contextMenu = Menu.buildFromTemplate([{ label: 'Restore', type: 'radio' }])
-  appIcon.setToolTip('Electron.js App')
-  appIcon.setContextMenu(contextMenu)
 }
 
 // This method will be called when Electron has finished
@@ -61,7 +36,8 @@ app.on('ready', createWindow)
 // explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
-    app.quit()
+    // app.quit()
+    mainWindow = null
   }
 })
 
@@ -72,6 +48,77 @@ app.on('activate', () => {
     createWindow()
   }
 })
+let trayIcon = null
+
+app.on('ready', () => {
+  // if (trayIcon) return mainWindow.hide()
+  trayIcon = new Tray(path.join('', './src/logo/bee.png'))
+
+  const template = [
+    {
+      label: 'bee-portal by JV',
+      enabled: false,
+    },
+
+    {
+      label: 'Open',
+      click: function () {
+        // mainWindow.show()
+        if (mainWindow) mainWindow.show()
+        else {
+          createWindow()
+        }
+      },
+    },
+    {
+      label: 'Exit',
+      click: function () {
+        app.quit()
+      },
+    },
+  ]
+
+  let trayMenu = Menu.buildFromTemplate(template)
+  trayIcon.setContextMenu(trayMenu)
+  trayIcon.setToolTip('bee-portal')
+
+  // mainWindow.hide()
+  mainWindow.on('closed', (event) => {
+    if (!app.isQuiting) {
+      event.preventDefault()
+      // mainWindow.hide()
+    }
+
+    return false
+  })
+})
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and import them here.
+// app.whenReady().then(() => {
+//   let trayIcon = new Tray(path.join('', './src/logo/bee.png'))
+
+//   const template = [
+//     {
+//       label: 'bee-portal by JV',
+//       enabled: false,
+//     },
+
+//     {
+//       label: 'Open',
+//       click: function () {
+//         mainWindow.show()
+//       },
+//     },
+//     {
+//       label: 'Exit',
+//       click: function () {
+//         app.quit()
+//       },
+//     },
+//   ]
+
+//   let trayMenu = Menu.buildFromTemplate(template)
+//   trayIcon.setContextMenu(trayMenu)
+//   trayIcon.setToolTip('bee-portal')
+// })
